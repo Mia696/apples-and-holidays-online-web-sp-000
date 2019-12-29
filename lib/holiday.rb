dfa2b1dfdf48846368bc46cf4568b726fb119631
Mia <tamiapaschall@gmail.com>
2019-12-29 23:44:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |k,v| #returning a bunch of nils with .map
 puts "#{k.capitalize}:"
    v.each do |x,y|
-  #   New Years: Party Hats	      title = x.to_s.tr("_", " ").split #.tr("_", " ") is new and looks like it replaces characters
+   title = x.to_s.tr("_", " ").split #.tr("_", " ") is new and looks like it replaces characters
   # Summer:	      puts "  #{title.collect {|x| x.capitalize}.join(" ")}: #{y.join(", ")}" 
 end
 

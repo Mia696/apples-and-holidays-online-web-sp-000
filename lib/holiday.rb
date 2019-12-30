26c0f82bcbb807edc87f8d3897e4d1729e77ce20
Mia <tamiapaschall@gmail.com>
2019-12-30 00:09:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,7 +18,12 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  
+ holiday_hash.each do |season, holiday|
+    if season == :winter
+        holiday.each do |holiday, supplies|
+        supplies << supply
+      end
+    end 
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
